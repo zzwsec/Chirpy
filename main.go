a7e3ea51fa5b8d8ecc744d46434faecb95e91159
zzwsec <zzwsec@163.com>
2026-01-29 10:48:40 +0800

    add fileserver handler

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	sm := http.NewServeMux()
+	sm.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: sm,
 	}
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
