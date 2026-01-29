cd8f7fefef002e33d60e9b604dcea8be3104c7e6
zzwsec <zzwsec@163.com>
2026-01-29 16:03:27 +0800

    add metrics and reset interface

--- main.go
+++ main.go
@@ -1,24 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
+	cfg := &apiConfig{fileserverHits: atomic.Int32{}}
 	sm := http.NewServeMux()
-
-	// 1. 将 /app/ 映射到当前目录
-	fileServer := http.FileServer(http.Dir("."))
-	sm.Handle("/app/", http.StripPrefix("/app", fileServer))
-
-	// 2. 注册接口
+	sm.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	sm.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
-
+	sm.HandleFunc("/metrics", cfg.handlerMetrics)
+	sm.HandleFunc("/reset", cfg.handlerReset)
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: sm,
